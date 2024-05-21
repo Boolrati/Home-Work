f2a441b78b7b1998e528ecf5f457055fbe216cbe
Igor Andros <boolrati@mail.ru>
2024-05-21 20:05:35 +0300

    commit 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,79 +1,148 @@
 class HomeWork {
-    public static void main(String args[]) {
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
-        posNegative();
-        countDown();
-        isLeapYear();
-    }
-    //задача 1
-    public static void printThreeWords()
-    {
-        System.out.println("3 слова в столбец метода printThreeWords(): \n");
-        System.out.println("Orange\nBanana\nApple\n");
-    }
-    //задача 2
-    public static void checkSumSign() {
-        int a = 15;
-        int b = 77;
-        System.out.print("Значение метода checkSumSign(): ");
-        if (a + b >= 0) System.out.println("Сумма положительная\n");
-        else System.out.println("Сумма отрицательная\n");
-    }
-    //задача 3
-    public static void printColor() {
-        int a = 101;
-        System.out.print("Значение метода printColor(): ");
-        if (a<=0) System.out.println("Красный\n");
-        else if (a>0 & a <=100) System.out.println("Желтый\n");
-        else if (a>100) System.out.println("Зеленый\n");
-    }
-    //задача 4
-    public static void compareNumbers() {
-        int a=-4;
-        int b=-5;
-        System.out.print("Значение метода compareNumbers(): ");
-        if (a>=b) System.out.println("a>=b\n");
-        else System.out.println("a<b\n");
+    //Задание 1
+
+    public static String printThreeWords(String words) {
+        return "Три слова в столбец: " + words;
     }
 
-    //задача 5
-    static boolean sumInRange(int a, int b) {
-        int sum = a + b;
-        return (sum >= 10 && sum <= 20);
+    //Задание 2
+
+    public static boolean checkSumSign(int a, int b) {
+        if (a + b >= 0) return true;
+        else return false;
     }
 
-    //задача 6
-    public static void posNegative() {
-        int a = 0;
-        if (a >= 0) System.out.println ("Число положительное\n");
-        else System.out.println("Число отрицательное\n");
+    //Задание 3
+
+    private static int printColor(int a) {
+        //return "Значение метода printColor(): " + a;
+        return a;
     }
-    //задача 7
+
+    //Задание 4
+    public static boolean compareNumbers(int a, int b) {
+        if (a >= b) return true;
+        else return false;
+    }
+
+    //Задание 5
+    public static boolean sumInRange(int a, int b) {
+        if (a + b >= 10 && a + b <= 20) return true;
+        else return false;
+    }
+
+    //Задание 6
+    public static boolean posNegative(int a) {
+        if (a >= 0) return true;
+        else return false;
+    }
+
+    //Задание 7
     public static boolean trueNegative(int a) {
-        if (a < 0) {
-            return true;
+        if (a < 0) return true;
+        else return false;
+    }
+
+    //Задание 8 - не решенное
+    public static void counterPrint(String text, int times) {
+        for (int a = 0; a < times; a++) {
+            //return text, times;
+            System.out.println(text);
         }
-        return false;
-
-    }
-    //задача 8
-    public static void countDown() {
-        for (int x = 10; x > 0; x--)
-            System.out.println("Обратный отсчет: " + x);
-    }
-    //задача 9
-    public static void isLeapYear(){
-        int year=2024;
-        System.out.println(isLeapYear(year));
-        //System.out.println("\n");
-    }
-    public static boolean isLeapYear(int year) {
-        year = 2020;
-        return (year % 4 == 0 && year % 100 == 0 && year % 400 == 0);
+        //return text;
     }
 
+    //Задание 9
+    public static boolean isLeapYear(int y) {
+        boolean year4 = (y % 4 == 0);
+        boolean yearHundred = (y % 100 != 0);
+        boolean year4Hundred = (y % 400 ==0);
+        if (year4 || yearHundred && year4Hundred) {
+            return true;
+        } else return false;
+    }
 
+    //Задание 10
+    public static void arraySwap() {
+        int arr[] = new int[10];
+    }
+
+
+
+    //Далее следует вывод в командную строку решений задач путем вызова из метода main
+    public static void main(String[] args) {
+
+        //вывод в командную строку решения задания 1
+        System.out.print("ЗАДАНИЕ 1: ");
+        String words = printThreeWords("\nOrange\nBanana\nApple\n");
+        System.out.println(words);
+
+        //вывод в командную строку решения задания 2
+        System.out.println("ЗАДАНИЕ 2:");
+        boolean c = checkSumSign(-4, 3);
+        if (c == true) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+
+        //выводим в командную строку решение задания 3
+        System.out.println("\nЗАДАНИЕ 3:");
+        int b = printColor(500);
+        if (b <= 0) System.out.println("Красный\n");
+        else if (b > 0 && b <= 100) System.out.println("Желтый\n");
+        else if (b > 100) System.out.println("Зеленый\n");
+
+        //выводим в командную строку решение задания 4
+        System.out.println("Задание 4");
+        boolean dif = compareNumbers(-3, 3);
+        if (dif) {
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<b");
+        }
+
+        //выводим в командную строку решение задания 5
+        System.out.println("\nЗадание 5");
+        boolean range = sumInRange(5, 10);
+        System.out.println(range);
+
+        //выводим в командную строку решение задания 6
+        System.out.println("\nЗадание 6");
+        boolean num = posNegative(-50);
+        if (num) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+
+        //выводим в командную строку решение задания 7
+        System.out.println("\nЗадание 7");
+        boolean num1 = trueNegative(0);
+        if (num1) {
+            System.out.println(num1);
+        } else {
+            System.out.println(false);
+        }
+
+        //выводим в командную строку решение задания 8
+        //System.out.println("\nЗадание 8");
+        //x = counterPrint("Обратный отсчет: ", 9);
+        //for (int y = 0; y < x; y++) {
+        //System.out.println(x);
+
+        //}
+
+        //выводим в командную строку решение задания 9
+        System.out.println("\nЗадание 9");
+        boolean d = isLeapYear(800);
+        System.out.println(d);
+
+        //выводим в командную строку решение задания 10
+        System.out.println("\nЗадание 10");
+        //int[] arr1=arraySwap(); {
+
+        //}
+
+    }
 }
