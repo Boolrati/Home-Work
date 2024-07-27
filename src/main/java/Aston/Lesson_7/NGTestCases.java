e8c812375f62fe08ad8826a880949a13a4295689
Igor Andros <boolrati@mail.ru>
2024-07-27 14:07:44 +0300

    Homework lesson 7 commit 3

--- src/main/java/Aston/Lesson_7/NGTestCases.java
+++ src/main/java/Aston/Lesson_7/NGTestCases.java
@@ -1,7 +1,32 @@
 package Aston.Lesson_7;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class NGTestCases {
 
+    @Test
+    void number2() {
+        Factorial factorial = new Factorial();
+        int actual = factorial.number(2);
+        int expected = 2;
+        assertEquals(expected,actual);
+    }
+    @Test
+    void number5() {
+        Factorial factorial = new Factorial();
+        int actual = factorial.number(5);
+        int expected = 120;
+        assertEquals(expected,actual);
+    }
+
+    @Test()
+    void number0(){
+        Factorial factorial = new Factorial();
+        int actual = factorial.number(0);
+        int expected = 1;
+        assertEquals(expected,actual);
+    }
 
 
 }
