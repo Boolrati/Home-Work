ae30071d7d371481d31f86f8da8fd843be2b73e5
Igor Andros <boolrati@mail.ru>
2024-08-04 21:16:29 +0300

    Homework lesson 8 commit 1

---
+++ src/Test/java/Aston.Lesson_8/MTSBYTest.java
@@ -0,0 +1,8 @@
+package Aston.Lesson_8;
+
+import org.junit.jupiter.api.Test;
+
+public class MTSBYTest {
+    @Test
+
+}
--- src/main/java/Aston/Lesson_8/Main.java
+++ src/main/java/Aston/Lesson_8/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://google.com");
+        driver.get("https://mts.by");
+        driver.manage().window().maximize();
     }
 }
