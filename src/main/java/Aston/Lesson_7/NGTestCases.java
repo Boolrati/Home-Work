439bc4a43330b89ab79874c7cc043d0e9395c7d0
Igor Andros <boolrati@mail.ru>
2024-08-05 15:27:47 +0300

    Homework lesson 7 commit 3

--- src/main/java/Aston/Lesson_7/NGTestCases.java
+++ src/main/java/Aston/Lesson_7/NGTestCases.java
@@ -1,7 +1,7 @@
 package Aston.Lesson_7;
+import org.testng.annotations.Test;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.assertEquals;
 
 public class NGTestCases {
 
