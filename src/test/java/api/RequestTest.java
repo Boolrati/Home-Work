eb5649327d847bc2fe26a712bbe9c1c0606c8bbb
Igor Andros <boolrati@mail.ru>
2024-09-06 21:18:37 +0300

    Homework lesson 10 commit 3

--- src/test/java/api/RequestTest.java
+++ src/test/java/api/RequestTest.java
@@ -1,152 +1,129 @@
 package api;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class RequestTest {
-    private final static String URL = "https://postman-echo.com/";
+    @BeforeAll
+    public static void setupRequestSpecification() {
+        requestSpecification = RestAssured.given()
+                .baseUri("https://postman-echo.com/");
+        responseSpecification = RestAssured.expect();
+        responseSpecification.body("headers.host", equalTo("postman-echo.com"));
+        responseSpecification.body("headers.x-forwarded-proto", equalTo("https"));
+        responseSpecification.body("headers.connection", equalTo("close"));
+        responseSpecification.body("headers.x-forwarded-port", equalTo("443"));
+        responseSpecification.body("headers.accept", equalTo("*/*"));
+        responseSpecification.body("headers.accept-encoding", equalTo("gzip,deflate"));
+        responseSpecification.statusLine("HTTP/1.1 200 OK");
+    }
 
     @Test
     public void checkGetRequest() {
-        given()
+        RestAssured
+                .given()
+                .spec(requestSpecification)
                 .when()
-                .get(URL + "get")
+                .get("get")
                 .then().log().all()
-                .body("headers.connection", equalTo("close"))
-                .body("headers.host", equalTo("postman-echo.com"))
-                .body("headers.x-forwarded-port", equalTo("443"))
-                .body("headers.accept", equalTo("*/*"))
-                .body("headers.accept-encoding", equalTo("gzip,deflate"))
-                .body("url", equalTo("http://postman-echo.com/get"))
-                .assertThat()
-                .statusCode(200);
+                .spec(responseSpecification)
+                .body("url", equalTo("https://postman-echo.com/get"));
     }
 
     @Test
     public void checkGetRequestWoops() {
-        given().queryParam("foo1", "bar1")
+        RestAssured
+                .given()
+                .queryParam("foo1", "bar1")
                 .queryParam("foo2", "bar2")
-                .when().get(URL + "get")
+                .spec(requestSpecification)
+                .when().get("get")
                 .then().log().all()
+                .spec(responseSpecification)
                 .body("args.foo1", equalTo("bar1"))
                 .body("args.foo2", equalTo("bar2"))
-                .body("headers.host", equalTo("postman-echo.com"))
-                .body("headers.x-forwarded-proto", equalTo("http"))
-                .body("headers.connection", equalTo("close"))
-                .body("headers.x-forwarded-port", equalTo("443"))
-                .body("headers.accept", equalTo("*/*"))
-                .body("headers.accept-encoding", equalTo("gzip,deflate"))
-                .body("url", equalTo("http://postman-echo.com/get?foo1=bar1&foo2=bar2"))
-                .assertThat()
-                .statusCode(200);
+                .body("url", equalTo("https://postman-echo.com/get?foo1=bar1&foo2=bar2"));
     }
 
     @Test
     public void checkPostRawText() {
         String requestBody = "    test" + ":" + " value";
-        given()
+        RestAssured
+                .given()
+                .spec(requestSpecification)
                 .body(requestBody)
-                .when().post(URL + "post")
+                .when().post("post")
                 .then().log().all()
+                .spec(responseSpecification)
                 .body("data", containsString(requestBody))
-                .body("headers.host", equalTo("postman-echo.com"))
-                .body("headers.x-forwarded-proto", equalTo("http"))
-                .body("headers.connection", equalTo("close"))
                 .body("headers.content-length", equalTo("15"))
-                .body("headers.x-forwarded-port", equalTo("443"))
-                .body("headers.accept", equalTo("*/*"))
-                .body("headers.content-type", equalTo("text/plain; charset=ISO-8859-1"))
-                .body("headers.accept-encoding", equalTo("gzip,deflate"))
-                .body("url", equalTo("http://postman-echo.com/post"))
-                .assertThat()
-                .statusCode(200);
+                .body("url", equalTo("https://postman-echo.com/post"));
     }
 
     @Test
     public void checkPostFormData() {
-        given()
+        RestAssured
+                .given()
+                .spec(requestSpecification)
                 .contentType("application/x-www-form-urlencoded; charset=utf-8")
                 .formParam("foo1", "bar1")
                 .formParam("foo2", "bar2")
-                .when().post(URL + "post")
+                .when().post("post")
                 .then().log().all()
+                .spec(responseSpecification)
                 .body("form.foo1", equalTo("bar1"))
                 .body("form.foo2", equalTo("bar2"))
-                .body("headers.host", equalTo("postman-echo.com"))
-                .body("headers.x-forwarded-proto", equalTo("http"))
-                .body("headers.connection", equalTo("close"))
                 .body("headers.content-length", equalTo("19"))
-                .body("headers.x-forwarded-port", equalTo("443"))
-                .body("headers.accept", equalTo("*/*"))
-                .body("headers.content-type", equalTo("application/x-www-form-urlencoded; charset=utf-8"))
-                .body("headers.accept-encoding", equalTo("gzip,deflate"))
-                .body("url", equalTo("http://postman-echo.com/post"));
+                .body("url", equalTo("https://postman-echo.com/post"));
     }
 
     @Test
     public void checkPutRawText() {
         String requestBodyText = "This is expected to be sent back as part of response body.";
-        given()
+        RestAssured
+                .given()
+                .spec(requestSpecification)
                 .body(requestBodyText)
-                .when().put(URL + "put")
+                .when().put("put")
                 .then().log().all()
+                .spec(responseSpecification)
                 .body("data", containsString(requestBodyText))
-                .body("headers.host", equalTo("postman-echo.com"))
-                .body("headers.x-forwarded-proto", equalTo("http"))
-                .body("headers.connection", equalTo("close"))
                 .body("headers.content-length", equalTo("58"))
-                .body("headers.x-forwarded-port", equalTo("443"))
-                .body("headers.accept", equalTo("*/*"))
-                .body("headers.content-type", equalTo("text/plain; charset=ISO-8859-1"))
-                .body("headers.accept-encoding", equalTo("gzip,deflate"))
-                .body("url", equalTo("http://postman-echo.com/put"))
-                .assertThat()
-                .statusCode(200);
+                .body("url", equalTo("https://postman-echo.com/put"));
     }
 
     @Test
     public void checkPatchRawText() {
         String requestBodyText = "This is expected to be sent back as part of response body.";
-        given()
+        RestAssured
+                .given()
+                .spec(requestSpecification)
                 .body(requestBodyText)
-                .when().patch(URL + "patch")
+                .when().patch("patch")
                 .then().log().all()
+                .spec(responseSpecification)
                 .body("data", containsString(requestBodyText))
-                .body("headers.host", equalTo("postman-echo.com"))
-                .body("headers.x-forwarded-proto", equalTo("http"))
-                .body("headers.connection", equalTo("close"))
                 .body("headers.content-length", equalTo("58"))
-                .body("headers.x-forwarded-port", equalTo("443"))
-                .body("headers.accept", equalTo("*/*"))
-                .body("headers.content-type", equalTo("text/plain; charset=ISO-8859-1"))
-                .body("headers.accept-encoding", equalTo("gzip,deflate"))
-                .body("url", equalTo("http://postman-echo.com/patch"))
-                .assertThat()
-                .statusCode(200);
+                .body("url", equalTo("https://postman-echo.com/patch"));
     }
 
     @Test
     public void checkDeleteRawText() {
         String requestBodyText = "This is expected to be sent back as part of response body.";
-        given()
+        RestAssured
+                .given()
+                .spec(requestSpecification)
                 .body(requestBodyText)
-                .when().delete(URL + "delete")
+                .when().delete("delete")
                 .then().log().all()
+                .spec(responseSpecification)
                 .body("data", containsString(requestBodyText))
-                .body("headers.host", equalTo("postman-echo.com"))
-                .body("headers.x-forwarded-proto", equalTo("http"))
-                .body("headers.connection", equalTo("close"))
                 .body("headers.content-length", equalTo("58"))
-                .body("headers.x-forwarded-port", equalTo("443"))
-                .body("headers.accept", equalTo("*/*"))
-                .body("headers.content-type", equalTo("text/plain; charset=ISO-8859-1"))
-                .body("headers.accept-encoding", equalTo("gzip,deflate"))
-                .body("url", equalTo("http://postman-echo.com/delete"))
-                .assertThat()
-                .statusCode(200);
+                .body("url", equalTo("https://postman-echo.com/delete"));
     }
 }
